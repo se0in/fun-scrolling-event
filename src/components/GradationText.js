500c0c59b4804350301fc11fa1e563a2b6681721
se0in <tjfls0@naver.com>
2023-11-27 18:07:59 +0900

    -임시저장, GradationText.js 이벤트 수정 중

--- src/components/GradationText.js
+++ src/components/GradationText.js
@@ -4,22 +4,42 @@
   * 스크롤에 따라 회전하며 화면을 꽉 채우는 이벤트 
 */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../scss/GradationText.scss'
+import { debounce } from 'lodash';
 
 const GradationText = () => {
+  const colorListRef = useRef([]);
 
-
+  // * 스크롤 효과 
   useEffect(() => {
     const rotateHandleScroll = () => {
       const isMobile = window.innerWidth < 1024
 
 
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      // console.log('scrollTop: ', scrollTop);
+      const content = document.querySelector(".gradation-text-content");
+      const contentHeight = content.getBoundingClientRect().top;
+      // console.log('contentHeight: ', contentHeight);
+
+      // const colorList = colorListRef.current;
+      // console.log('colorList: ', colorList);
+      // const gradationBackgroundColorListScroll = colorList.getBoundingClientRect();
+      // console.log('gradationBackgroundColorListScroll: ', gradationBackgroundColorListScroll);
+
       const inner = document.querySelector(".gradation-inner");
       const innerHeight = inner.getBoundingClientRect().top;
+      // console.log('innerHeight: ', innerHeight);
       const phone = document.querySelector(".phone");
       const gradationBackgroundColorUl = document.querySelector(".gradation-background-color");
+      const colorScroll = gradationBackgroundColorUl.getBoundingClientRect();
+
+      const gradationBackgroundColorList = document.querySelector(".gradation-background-color li");
+      const gradationBackgroundColorListScroll = gradationBackgroundColorList.getBoundingClientRect();
+
+      const width = gradationBackgroundColorListScroll.width;
+
       const gradationBackgroundTextUl = document.querySelector(".gradation-background-text");
 
       // * 스크롤, 회전 위치에 따라 스타일 변화될 부분
@@ -44,7 +64,7 @@ const GradationText = () => {
       }
 
       if (isInsideViewPort) {
-        gradationBackgroundColorUl.style.pointerEvents = 'none';
+        // gradationBackgroundColorUl.style.pointerEvents = 'none';
 
         if (isRotationDiagonal) {
           phone.style.width = `${widthDefault + (phoneWidth * 12)}vw`;
@@ -58,17 +78,33 @@ const GradationText = () => {
           // * 공통된 스타일 삼항 
           phone.style.border = isRotationMostly ? 'none' : '10px solid #fff';
           phone.style.borderRadius = isRotationMostly ? '0' : '54px';
-          phone.style.overflow = isRotationMostly ? 'visible' : 'hidden';
+
 
           if (isRotationMostly) {
-            // * 90도로 회전 완료 후 화면 채울 스타일
+            // * 80도로 회전 완료 후 화면 채울 스타일
             phone.style.width = '100vh';
             phone.style.height = '100vw';
 
-            if (isRotationComplete && isInsideViewPort) {
-              phone.style.overflowX = 'scroll';
-              gradationBackgroundColorUl.style.pointerEvents = 'auto';
+
+            // document.body.style.pointerEvents = "none"
+            phone.style.overflow = isRotationComplete ? 'visible' : 'hidden';
+            // * 90도로 회전 완료 후 텍스트 영역 표시, 스크롤
+            const test = gradationBackgroundColorUl.getBoundingClientRect().left
+            const scrollXWidth = window.innerWidth + (test * -1)
+            console.log('scrollXWidth: ', scrollXWidth);
+            
+            phone.style.overflowX = 'scroll';
+            if (isRotationComplete && scrollXWidth <= 3800) {
               gradationBackgroundTextUl.style.display = 'block';
+              phone.style.pointerEvents = "auto"
+              console.log('이 영역에서 바디 스크롤 막아야함');
+              
+              /* if (scrollXWidth <= 3800) {
+                document.body.style.pointerEvents = "none"
+              }else {
+                document.body.style.pointerEvents = "auto"
+
+              } */
             }
 
           } else {
@@ -85,37 +121,31 @@ const GradationText = () => {
       }
 
     };
+
     window.addEventListener('scroll', rotateHandleScroll);
     return () => {
       window.removeEventListener('scroll', rotateHandleScroll);
     };
   });
-
-
-
-
+  
   return (
     <div className='gradation-text-content'>
       <div className='gradation-inner'>
         <div className='phone-box'>
           <div className='phone'>
             <ul className='gradation-background-color'>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+              {/* 요소 반복 */}
+              {[...Array(6)].map((_, index) => (
+                <li key={index} ref={(element) => (colorListRef.current[index] = element)}></li>
+              ))}
             </ul>
           </div>
         </div>
         <ul className='gradation-background-text'>
-          <li><p>Gradient effect!!</p></li>
-          <li><p>Gradient effect!!</p></li>
-          <li><p>Gradient effect!!</p></li>
-          <li><p>Gradient effect!!</p></li>
-          <li><p>Gradient effect!!</p></li>
-          <li><p>Gradient effect!!</p></li>
+          {/* 요소 반복 */}
+          {[...Array(6)].map((_, index) => (
+            <li key={index}><p>Gradient effect!!</p></li>
+          ))}
         </ul>
       </div>
     </div>
